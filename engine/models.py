5c895c90b6908bf6511dee87ef714298b487063f
Eddy Almuiña <almuina.e@gmail.com>
2017-12-07 22:43:26 -0500

    showing queries' results

--- engine/models.py
+++ engine/models.py
@@ -2,26 +2,7 @@ from django.db import models
 
 
 class Document(models.Model):
+    filename = models.CharField(max_length=140, unique=True)
+    title = models.CharField(max_length=140)
+    content = models.TextField(max_length=280)
     path = models.FilePathField(unique=True)
-
-
-class Term(models.Model):
-    name = models.CharField(unique=True, max_length=140)
-
-
-class Correlation(models.Model):
-    term1 = models.ForeignKey(Term, on_delete=models.CASCADE, related_name='correlation_set_term1')
-    term2 = models.ForeignKey(Term, on_delete=models.CASCADE, related_name='correlation_set_term2')
-    k = models.FloatField()
-
-    class Meta:
-        unique_together = ('term1', 'term2')
-
-
-class Occurrence(models.Model):
-    term = models.ForeignKey(Term, on_delete=models.CASCADE)
-    document = models.ForeignKey(Document, on_delete=models.CASCADE)
-    weight = models.FloatField()
-
-    class Meta:
-        unique_together = ('term', 'document')
--- engine/views.py
+++ engine/views.py
@@ -1,10 +1,28 @@
+import pathlib
+
 from django.shortcuts import render, HttpResponse
 
+from engine.models import Document
 import engine.modules.ui as ui
 
 
 def build(request):
     path = request.GET.get('path')
+    bulk = []
+    for doc in pathlib.Path(path).iterdir():
+        if doc.name == 'index.json':
+            continue
+        with open(str(doc)) as file:
+            title = file.readline(140)
+            content = file.read(280)
+        bulk.append(Document(
+            path=str(doc),
+            filename=doc.name,
+            title=title,
+            content=content
+        ))
+    Document.objects.all().delete()
+    Document.objects.bulk_create(bulk)
     ui.build(path)
     return HttpResponse()
 
@@ -19,5 +37,5 @@ def search(request):
     response = ui.search(query, count)
     results = []
     if response['success']:
-        results = [(doc['document'], doc['match']) for doc in response['results']]
-    return render(request, 'engine/document_list.html', {'documents': results})
+        results = [(Document.objects.get(filename=doc['document']), doc['match']) for doc in response['results']]
+    return render(request, 'engine/document_list.html', {'query': query, 'documents': results})
